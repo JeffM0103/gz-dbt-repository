a57fa526790679b008d302c0f8ccce557bc3e8c5
Jeff <jf.meurgey@gmail.com>
2025-07-29 12:35:26 +0000

    create ope margin

--- models/int_orders_operational.sql
+++ models/int_orders_operational.sql
@@ -1,5 +1,8 @@
 SELECT
 orders_id,
 date_date,
-margin + shipping_fee - logcost - ship_cost AS operational_margin
-FROM {{"int_orders_margin"}}
+ROUND((margin + shipping_fee - logcost - CAST(ship_cost AS FLOAT64)),2) AS operational_margin
+FROM {{ref("int_orders_margin")}}
+JOIN {{ref("stg_raw__ship")}}
+USING (orders_id)
+ORDER BY orders_id DESC
